c4f2e9c3efcb34ef4aab218decbb7339ba33abe6
luckyboyroman <68152821+luckyboyroman@users.noreply.github.com>
2020-10-17 16:14:57 +0300

    Update Source.cpp

--- Perceptron/Source.cpp
+++ Perceptron/Source.cpp
@@ -14,7 +14,6 @@ struct neuron {
 
 class network {
 public:
-	int layers;
 	neuron** neurons;
 	double *** weights;
 	int* size;
@@ -26,9 +25,7 @@ public:
 		return res;
 	}
 	void SetLayers(int n,int *p) {
-		srand(time(NULL));
 		this->n = n;
-		layers = n;
 		neurons = new neuron * [n];
 		size = new int[n];
 		weights = new double** [n - 1];
@@ -40,7 +37,7 @@ public:
 				for (int j = 0; j < p[i]; j++) {
 					weights[i][j] = new double[p[i + 1]];
 					for (int k = 0; k < p[i + 1]; k++) {
-						weights[i][j][k] = (rand() % 40) / 10.0;
+						weights[i][j][k] = (rand() % 70) / 50.0;
 					}
 				}
 			}
@@ -69,20 +66,40 @@ public:
 			neurons[0][i].value = values[i];
 		}
 	}
-	void forward_feed(int LayerNumber) {
-		for (int i = 0; i < size[LayerNumber]; i++) {
-			for (int j = 0; j < size[LayerNumber - 1]; j++) {
-				neurons[LayerNumber][i].value += neurons[LayerNumber - 1][j].value*weights[LayerNumber-1][j][i];
+	void forward_feed() {
+		for (int k = 1; k < n; k++) {
+			for (int i = 0; i < size[k]; i++) {
+				for (int j = 0; j < size[k - 1]; j++) {
+					neurons[k][i].value += neurons[k - 1][j].value * weights[k - 1][j][i];
+				}
+				neurons[k][i].act();
 			}
-			neurons[LayerNumber][i].act();
 		}
 	}
-	void BackPropogation(double pred,double expect,double lr) {
-		double weights_delta, error;
-
+	void BackPropogation(double expect) {
+		neurons[n - 1][0].error = expect - neurons[n - 1][0].value;
+		for (int i = n - 2; i > 0; i--) {
+			for (int j = 0; j < size[i]; j++) {
+				double error = 0.0;
+				for (int k = 0; k < size[i+1]; k++) {
+					error += neurons[i+1][k].error * weights[i][j][k];
+				}
+				neurons[i][j].error = error;
+			}
+		}
+	}
+	void WeightsUpdater(double lr) {
+		/*LayerNum=0*/
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < size[i]; j++) {
+				for (int k = 0; k < size[i + 1]; k++) {
+					weights[i][j][k] += neurons[i + 1][k].error * sigm_pro(neurons[i + 1][k].value) * neurons[i][j].value * lr;
+				}
+			}
+		}
 	}
 	};
-/*Îïðåäåëåíèÿ ô-öèé*/
+/*ÐžÐ¿Ñ€ÐµÐ´ÐµÐ»ÐµÐ½Ð¸Ñ Ñ„-Ñ†Ð¸Ð¹*/
 void network::ShowWeights(int* p){
 	cout << "Shows weights" << endl;
 	for (int i = 0; i < n - 1; i++) {
@@ -99,22 +116,25 @@ void network::ShowWeights(int* p){
 
 int main() {
 	network hi;
-	int const n = 3;
+	int const n = 4;
 	int* size = new int[n];
 	double* results = new double[n];
-	size[0] = 3;
-	size[1] = 2;
-	size[2] = 2;
+	size[0] = 1;
+	size[1] = 3;
+	size[2] = 3;
+	size[3] = 1;
 	double* input_data = new double[size[0]];
-	for (int i = 0; i < size[0]; i++) {
-		input_data[i] = i*0.5;
-	}
+	input_data[0] = 1.0;
 	hi.SetLayers(n,size);
 	hi.NeuronsCleaned();
 	hi.SetInput(input_data);
 	hi.Show(size);
 	hi.ShowWeights(size);
-	hi.forward_feed(1);
-	hi.forward_feed(2);
+	hi.forward_feed();
+	for (int i = 0; i < 5000; i++) {
+		hi.BackPropogation(50.0);
+		hi.WeightsUpdater(0.5);
+		hi.forward_feed();
+	}
 	hi.Show(size);
 }
